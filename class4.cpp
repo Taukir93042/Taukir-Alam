b09029ed4661c0cbc625b87646dccf4c65121156
Taukir93042 <167509443+Taukir93042@users.noreply.github.com>
2024-05-13 14:45:28 +0530

    Add files via upload

---
+++ class4.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class set{
+int m,n;
+public:
+    void input(void);
+    void display(void);
+    int largest(void);
+};
+int set :: largest(void){
+    if(m>=n)
+        return(m);
+    else
+        return(n);
+}
+    void set :: input(void){
+        cout << "Input values of m & n" <<"\n";
+        cin >>m >>n;
+               }
+    void set :: display(void){
+        cout<<"Largest value is=" <<largest()<<"\n";
+}
+  int main(){
+        set A;
+        A.input();
+        A.display();
+        return 0;
+}
