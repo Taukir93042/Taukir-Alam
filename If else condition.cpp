57da9671be9e9c73f8784fd68b4b11382f4a1060
Taukir93042 <167509443+Taukir93042@users.noreply.github.com>
2024-05-13 13:10:03 +0530

    Add files via upload

---
+++ If else condition.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()  //Start program
+{
+    int a;  ////Variable declaration
+    int b; //Variable declaration
+    cout<<"Enter value of A\n";
+    cin>>a;
+    cout<<"Enter value of B\n";
+    cin>>b;
+    if(a>b)  //Conditional Statement
+    {
+        cout<<"A is greater"; //print
+    }
+    else
+    {
+        cout<<"B is greater";
+    }
+   return 0;
+}
